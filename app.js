57b2f56336d0860012033adbc7d529e46d4a1abf
banlong <nghiepnds@yahoo.com>
2016-11-14 18:45:07 -0800

    Add : 1) create Line Chart & Bubble Chart picture base on input data 2) API defined

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ var users = require('./routes/users');
 var charts = require('./routes/charts');
 var graph = require('./routes/graph');
 
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'cut2it.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,8 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/charts', charts);
-app.use('/graph', graph);
+app.use('/api/chart', charts);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
